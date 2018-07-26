49a80c1b97a8e0713f77cf619e72c232a48aa8f5
Mario Orlandi <morlandi@brainstorm.it>
2018-07-26 15:31:19 +0200

    initial

---
+++ backend/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from adminsortable2.admin import SortableInlineAdminMixin
+from .models import Button
+from .models import Panel
+
+
+class ButtonTabularInline(SortableInlineAdminMixin, admin.TabularInline):
+    # We don't use the Button model but rather the juction model specified on Panel.
+    model = Panel.buttons.through
+    extra = 0
+    autocomplete_fields = ['button', ]
+
+
+class PanelTabularInline(admin.TabularInline):
+    # We don't use the Button model but rather the juction model specified on Panel.
+    model = Panel.buttons.through
+    exclude = ['button_order', ]
+    extra = 0
+    autocomplete_fields = ['panel', ]
+
+
+@admin.register(Panel)
+class PanelAdmin(admin.ModelAdmin):
+    inlines = (ButtonTabularInline,)
+    search_fields = ['name', ]
+
+
+@admin.register(Button)
+class ButtonAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'button_text', )
+    inlines = (PanelTabularInline,)
+    search_fields = ['name', 'button_text', ]
---
+++ backend/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Button(models.Model):
+    """A button"""
+    name = models.CharField(max_length=64)
+    button_text = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Panel(models.Model):
+    """A Panel of Buttons - this represents a control panel."""
+    name = models.CharField(max_length=64)
+    buttons = models.ManyToManyField(Button, through='PanelButtons')
+
+    def __str__(self):
+        return self.name
+
+
+class PanelButtons(models.Model):
+    """This is a junction table model that also stores the button order for a panel."""
+    panel = models.ForeignKey(Panel, on_delete=models.CASCADE)
+    button = models.ForeignKey(Button, on_delete=models.CASCADE)
+    button_order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ('button_order',)
+
+    def __str__(self):
+        return ''
